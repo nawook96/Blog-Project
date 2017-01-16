406b9cabfea52ee2bb1545b8acc7a354af12cada
Dongwook Shin <nawook0331@nate.com>
2017-01-16 18:42:08 +0900

    big 변화

---
+++ Blog/Login.php
@@ -0,0 +1,40 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+<link rel="stylesheet" href="./css/bootstrap.min.css">
+<link rel="stylesheet" href="./css/bootstrap-theme.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+<script src="./js/bootstrap.min.js"></script>
+</head>
+<body>
+  <div class="container">
+  	<div class="row">
+          <div class="col-md-4 col-md-offset-4">
+              <div class="panel panel-default">
+                  <div class="panel-heading">
+                      <h3 class="panel-title">Welcome to Admin's Blog</h3>
+                  </div>
+                  <div class="panel-body">
+                      <form role="form">
+                          <fieldset>
+                              <div class="form-group">
+                                  <input class="form-control" placeholder="ID" name="id" type="id" autofocus>
+                              </div>
+                              <div class="form-group">
+                                  <input class="form-control" placeholder="Password" name="password" type="password" value="">
+                              </div>
+                              <!-- Change this to a button or input when using this as a form -->
+                              <button type="button" class="btn btn-info btn-block">로그인</button>
+                              <p>처음 이신가요? <a href="signUp.html" class="">회원 가입</a></p>
+                          </fieldset>
+                      </form>
+                  </div>
+              </div>
+          </div>
+      </div>
+  </div>
+</body>
+</html>
